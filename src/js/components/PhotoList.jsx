5bd03fade93090e88e01cb9713be716281347785
Taaha Chaudhry <taaha.chaudhry@idean.com>
2016-03-07 09:59:13 -0800

    Styled list of photos and implemented previous and next buttons

--- src/js/components/PhotoList.jsx
+++ src/js/components/PhotoList.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import Button from './Button'
+import classnames from 'classnames'
 
 class PhotoList extends React.Component {
   constructor(props, context) {
     super(props, context)
   }
 
-  searchNextPage(e) {
+  searchPreviousPage() {
+    const keyword = this.props.photos.keyword;
+    const tag = this.props.photos.tag;
+    const page = this.props.photos.page-=1;
+    this.props.actions.fetchPhotos(keyword, tag, page);
+  }
+
+  searchNextPage() {
     const keyword = this.props.photos.keyword;
     const tag = this.props.photos.tag;
     const page = this.props.photos.page+=1;
-    if (e.type === 'click') {
-      this.props.actions.fetchPhotos(keyword, tag, page);
-    }
+    this.props.actions.fetchPhotos(keyword, tag, page);
   }
 
   showGeo() {
@@ -22,19 +28,35 @@ class PhotoList extends React.Component {
   render() {
     return (
       <div>
-        {
-          this.props.photos['photos'].map((photo, index) => {
-            return (
-              <div key={index}>
-                <img src={photo.image_url} onClick={this.showGeo.bind(this)} id={index}/>
-              </div>
-            );
-          })
+        <ul className="photo-list">
+          {
+            this.props.photos['photos'].map((photo, index) => {
+              return (
+                <li className="photo-item" key={index} id={index}>
+                  <div className="image-wrapper">
+                    <img src={photo.image_url} onClick={this.showGeo.bind(this)} id={index}/>
+                  </div>
+                </li>
+              );
+            })
           }
-          <Button
-            onClick={this.searchNextPage.bind(this)}>
-            Next
-          </Button>
+          <li className="pagination-buttons">
+            <Button
+              onClick={this.searchPreviousPage.bind(this)}
+              className={classnames({
+                  disabled: this.props.photos.page === 1
+              })}>
+              &lt;
+            </Button>
+            <Button
+              onClick={this.searchNextPage.bind(this)}
+              className={classnames({
+                  disabled: this.props.photos['photos'].length < 21
+              })}>
+              &gt;
+            </Button>
+          </li>
+        </ul>
       </div>
     );
   }
